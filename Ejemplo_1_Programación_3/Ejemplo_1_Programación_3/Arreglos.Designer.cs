611baa8dcf7e813f50635ab53c26a9be38da441d
ISMa1814 <98503060+ISMa1814@users.noreply.github.com>
2022-02-11 02:14:27 -0600

    Clase 6
    Vimos los Arreglos, Vectores

---
+++ Ejemplo_1_Programación_3/Ejemplo_1_Programación_3/Arreglos.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejemplo_1_Programación_3
+{
+    partial class Arreglos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(302, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 184);
+            this.listBox1.TabIndex = 0;
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(330, 50);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(111, 44);
+            this.EjecutarButton.TabIndex = 1;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // Arreglos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Arreglos";
+            this.Text = "Arreglos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button EjecutarButton;
+    }
+}
---
+++ Ejemplo_1_Programación_3/Ejemplo_1_Programación_3/Arreglos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_1_Programación_3
+{
+    public partial class Arreglos : Form
+    {
+        public Arreglos()
+        {
+            InitializeComponent();
+        }
+
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            //Vetores
+            int[] arreglo = new int[5];
+
+            arreglo[0] = 40;
+            arreglo[1] = 10;
+            arreglo[2] = 30;
+            arreglo[3] = 60;
+            arreglo[4] = 80;
+
+            int[] arreglo2 = new int[6] { 41, 63, 45, 68, 74, 99 };
+
+            int[] arreglo3 = new int[] { 2, 5, 1, 3, 7, 6 };
+
+            int[] arreglo4 = { 5, 6, 7, 4, 3, 5, 7 };
+
+            string[] arreglo5 = { "Lunes", "Marte", "Miercoles", "Jueves", "Viernes" };
+
+            /*for (int i = 0; i < arreglo5.Length; i++)
+            {
+                listBox1.Items.Add(arreglo5[i]);
+            }
+            */
+
+            //Esta es otra manera de mostrar un arreglo
+            foreach (var item in arreglo5)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
