1cbd9173faa11cc7a2db7682a34b40a81b86f946
ISMa1814 <98503060+ISMa1814@users.noreply.github.com>
2022-02-20 01:21:23 -0600

    Clase 10
    Hemos visto la Clase Libreria, vimos como se crean los metodos constructores, los atributos, etc.

---
+++ Ejemplo_1_Programación_3/ClasesLibreria/Coche.cs
@@ -0,0 +1,40 @@
+﻿namespace ClasesLibreria;
+
+public class Coche
+{
+    //Atributos van con minuscula
+    private int id;
+    //private string marca;
+    //private string modelo;
+    //private string precio;
+    //private string km;
+
+    //Las propiedades van con el mismo nombre de los atributos, solo que van con mayusculas
+    //Así es como se hacian antes
+    public int Id { get => id; set => id = value; }
+
+    //Así es como se hacen ahora, y de esta forma no es necesario crearle un atributo
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public decimal Precio { get; set; }
+    public int Km { get; set; }
+
+    //Contructor
+    //Este es el contructor por defecto y no es necesario declararlo, ya que C# ya lo tiene declarado
+    public Coche() 
+    { 
+    }
+
+    //Esta es una forma de declarar un Contructor
+    public Coche(int id, string marca)
+    {
+        Id = id;
+        Marca = marca;
+    }
+
+    //Metodos
+    public string DevolverDatosCoche()
+    {
+        return "ID: " + Id + " Marca: " + Marca + " Modelo: " + Modelo;
+    }
+}
---
+++ Ejemplo_1_Programación_3/Ejemplo_1_Programación_3/LlamarClases.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClasesLibreria;
+//Aquí agregamos la Clases que creamos
+
+namespace Ejemplo_1_Programación_3
+{
+    public partial class LlamarClases : Form
+    {
+        public LlamarClases()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Aqui utilizamos el Contructor que creamos
+            Coche coche = new Coche(10, "Toyota");
+
+            //Sino creamos un contructor podemos definir el objeto de esta forma
+            coche.Modelo = "22R";
+            coche.Precio = Convert.ToDecimal(100000.00);
+            coche.Km = 150000;
+
+            MessageBox.Show(coche.DevolverDatosCoche());
+        }
+    }
+}
